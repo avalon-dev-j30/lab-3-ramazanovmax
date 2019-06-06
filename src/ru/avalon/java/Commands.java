d8db7824410fbbff25865be962a8ec1a5a5282be
maximramazanov <max.dag.pen@yandex.ru>
2019-06-06 17:00:32 +0300

    Лабараторная работа 3 j30

--- src/ru/avalon/java/Commands.java
+++ src/ru/avalon/java/Commands.java
@@ -4,10 +4,31 @@ package ru.avalon.java;
  * Обрабатываемые приложением команды.
  */
 public enum Commands {
-    move,
-    copy,
-    exit
+    move("Переименование/Перемещение"),
+    copy("Копирование"),
+    delete("Удаление"),
+    list("Список файлов в разделе"),
+    exit("Выход"),
+    help("Отображение доступных комманд"),;
     /*
      * TODO №8 К текущему списку команд, добавьте ещё две команды
      */
+    private final String name;
+    Commands(String s) {
+
+        name = s;
+    }
+    public String getValue() {//метод для получения значения
+        return name;
+    }
+
+
+    public static String getCommands(){
+        String s = "Доступные комманды:\n";
+        for(Commands c : Commands.values()) {
+            s += c + " - " + c.getValue() + "\n";
+        }
+        return s;
+    }
+
 }
--- src/ru/avalon/java/Lab3.java
+++ src/ru/avalon/java/Lab3.java
@@ -1,8 +1,12 @@
 package ru.avalon.java;
 
+import ru.avalon.java.actions.FileCopyAction;
+import ru.avalon.java.actions.FileDeleteAction;
+import ru.avalon.java.actions.FileListAction;
+import ru.avalon.java.actions.FileMoveAction;
 import ru.avalon.java.console.ConsoleUI;
 
-import java.io.IOException;
+import java.io.*;
 
 /**
  * Лабораторная работа №3
@@ -15,12 +19,14 @@ import java.io.IOException;
  * @author Daniel Alpatov <danial.alpatov@gmail.com>
  */
 public class Lab3 extends ConsoleUI<Commands> {
+    private BufferedReader cmdReader = new BufferedReader(new InputStreamReader(System.in));
     /**
      * Точка входа в приложение.
      * 
      * @param args 
      */
     public static void main(String[] args) {
+        System.out.println("Введите help для получения списка комманд");
         new Lab3().run();
     }
     /**
@@ -38,23 +44,54 @@ public class Lab3 extends ConsoleUI<Commands> {
      */
     @Override
     protected void onCommand(Commands command) throws IOException {
+        String n1, n2;
         switch (command) {
             case copy:
                 /*
                  * TODO №6 Обработайте команду copy
                  */
+                System.out.print("Имя файла: >");
+                n1 = cmdReader.readLine();
+                System.out.print("Копировать в: >");
+                n2 = cmdReader.readLine();
+
+                new FileCopyAction(n1, n2).start();
                 break;
             case move:
                 /*
                  * TODO №7 Обработайте команду move
                  */
+                System.out.print("Имя файла: ");
+                n1 = cmdReader.readLine();
+                System.out.print("to: ");
+                n2 = cmdReader.readLine();
+                new FileMoveAction(n1, n2).start();
+                break;
+            case delete:
+                /*
+                 * TODO №7 Обработайте команду delete
+                 */
+                System.out.print("Имя удаляемого: ");
+                n1 = cmdReader.readLine();
+                new FileDeleteAction(n1).start();
                 break;
             case exit:
                 close();
+                System.exit(0);
                 break;
                 /*
                  * TODO №9 Обработайте необработанные команды
                  */
+            case list:
+                System.out.print("Введите название раздела или нажмите enter:");
+                n1 = cmdReader.readLine();
+                if(n1.length() == 0)n1 = ".";
+
+                new FileListAction(n1).start();
+                break;
+            case help:
+                    System.out.println(Commands.getCommands());
+                break;
         }
     }
     
